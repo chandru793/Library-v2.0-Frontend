bc0a199304fb3d4e27ff48b2f068e53492b3a04b
chandru793 <67958864+chandru793@users.noreply.github.com>
2023-04-23 18:24:31 +0530

    read books

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -6,13 +6,18 @@ import axios from "axios";
 export const getAllBooks = async () =>
   await axios.get("http://localhost:8081/api/v2/library");
 
+export const googlebooks = async () =>
+  await axios.get(
+    `https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`
+  );
+
 //=================================
 //=============News================
 
 //api to get all the news
 export const getAllNews = async (category, country) =>
   await axios.get(
-    `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=7d0ee033081b4145b43934b3a545ed35`
+    `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_NEWS_API}`
   );
 
 //================================
--- src/components/E-learning/Books.jsx
+++ src/components/E-learning/Books.jsx
@@ -4,7 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea } from '@mui/material';
-import { getAllBooks } from '../Api/Api';
+import { getAllBooks, googlebooks } from '../Api/Api';
 import { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
 import '../../assets/css/Books.css'
@@ -16,40 +16,48 @@ export default function Books() {
     const [library, setLibrary] = useState([]);
 
     useEffect(() => {
-        getAllBooks().then((res) => {
-            console.log(res.data.books);
-            setLibrary(res.data.books);
+        // getAllBooks().then((res) => {
+            // console.log(res.data.books);
+            // setLibrary(res.data.books);
+        // });
+        googlebooks().then((res) => {
+            console.log(res.data);
+            console.log(res.data.items[0].volumeInfo.title);
+            setLibrary(res.data.items);
         });
     }, []);
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className='bookContainer'>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                     {
                         library.map((book) =>
                             <Grid item xs={4} sm={4} md={4} key={book.id} >
-                                <Card sx={{ maxWidth: 400 }} className='cards' key={book._id}>
+                                <Card sx={{ maxWidth: 400 }} className='cards' key={book.accessInfo.id}>
                                     <CardActionArea style={{ "height": "500px", "display": "flex", "flexDirection": "column", "justifyContent": "space-between" }}>
                                         <CardMedia
                                             component="img"
                                             height="250"
-                                            image={book.image}
+                                            image={book.volumeInfo.imageLinks.thumbnail}
                                             alt="green iguana"
                                         />
                                         <CardContent className='card'>
                                             <Typography gutterBottom variant="h5" component="div">
-                                                {book.bookName}
+                                                {book.volumeInfo.title}
                                             </Typography>
                                             <Typography gutterBottom variant="h6" component="div">
-                                                {book.authorName}
+                                                ~ {book.volumeInfo.authors}
                                             </Typography>
-                                            <Typography variant="body2" color="text.secondary" className='card'>
+                                            {/* <Typography variant="body2" color="text.secondary" className='card'>
                                                 {book.bookDescription}
-                                            </Typography>
+                                            </Typography> */}
                                         </CardContent>
                                         <CardContent style={{ "display": "flex", "alignItems": "center", "justifyContent": "center" }}>
-                                            <Button size="small" className='button'>
+                                            <Button
+                                                size="small"
+                                                className='button'
+                                                onClick={() => { window.open(book.volumeInfo.previewLink, '_blank') }}>
                                                 Read Book
                                             </Button>
                                         </CardContent>
