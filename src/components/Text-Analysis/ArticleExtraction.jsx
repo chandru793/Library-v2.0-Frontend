ed4eebb6388909ebdf290860449e56ff45b5584e
R Chandru <67958864+chandru793@users.noreply.github.com>
2023-04-24 22:11:40 +0530

    Update ArticleExtraction.jsx

--- src/components/Text-Analysis/ArticleExtraction.jsx
+++ src/components/Text-Analysis/ArticleExtraction.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { articleExtraction } from '../Api/Api';
 import '../../assets/css/ArticleExtraction.css'
 
 //components
@@ -13,35 +14,55 @@ const ArticleExtraction = () => {
     const [text, setText] = useState("");
     const [show, setShow] = useState(true);
 
-    const Submit = (e) => {
-        const encodedParams = new URLSearchParams();
-        encodedParams.append("language", "english");
-        encodedParams.append("url", data);
-
+    const summarize = () => {
+        const dataObj = JSON.stringify({
+            language: "english",
+            summary_percent: 10,
+            text: text,
+        });
+        console.log(dataObj);
         const options = {
-            method: 'POST',
-            url: 'https://text-analysis12.p.rapidapi.com/article-extraction/api/v1.3',
+            method: "POST",
+            url: "https://text-analysis12.p.rapidapi.com/summarize-text/api/v1.1",
             headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'X-RapidAPI-Key': 'ab98824deemsh78644ac08a32df7p1f78eajsnc05f5a9483e0',
-                'X-RapidAPI-Host': 'text-analysis12.p.rapidapi.com'
+                "content-type": "application/json",
+                "X-RapidAPI-Key": "ab98824deemsh78644ac08a32df7p1f78eajsnc05f5a9483e0",
+                "X-RapidAPI-Host": "text-analysis12.p.rapidapi.com",
             },
-            data: encodedParams
+            data: dataObj,
         };
 
-        axios.request(options).then(function (response) {
-            console.log(response.data);
-            setTitle(response.data.article.title)
-            setSummary(response.data.article.summary)
-            setText(response.data.article.text)
-        }).catch(function (error) {
-            console.error(error);
+        axios
+            .request(options)
+            .then(function (response) {
+                // console.log(response.data);
+                setSummary(response.data.summary);
+                // console.log("summary", summary);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+    }
+
+    const update = () => {
+        articleExtraction(data).then((res) => {
+            // console.log(res.data);
+            setTitle(res.data.title)
+            // console.error("title",title)
+            setText(res.data.text)
+            // console.error("text",text)
+            summarize();
         });
     }
 
+    useEffect(() => {
+        update();
+    }, []);
+
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className='outerContainer'>
                 <div className='field'>
                     <input
@@ -52,9 +73,9 @@ const ArticleExtraction = () => {
                         placeholder='Enter url to extract article'
                         onChange={(e) => setData(e.target.value)}
                     />
-                    <button className='button1' onClick={Submit}>Submit</button>
+                    <button className='button1' onClick={() => update(data)}>Submit</button>
                 </div>
-                {show && title !== "" && summary !== "" && text !== "" ? (
+                {show && title !== "" && summary !== "" && text !== ""? (
                     <div className='result'>
                         <h1 className='h1'>{title}</h1>
                         <h3>Summary</h3>
