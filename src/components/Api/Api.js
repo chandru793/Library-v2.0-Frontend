c95e4b491bea67f54bab151bf32d14c6e95ab6aa
chandru793 <67958864+chandru793@users.noreply.github.com>
2023-04-24 21:22:20 +0530

    books and extraction update

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -6,9 +6,9 @@ import axios from "axios";
 export const getAllBooks = async () =>
   await axios.get("http://localhost:8081/api/v2/library");
 
-export const googlebooks = async () =>
+export const googlebooks = async (search) =>
   await axios.get(
-    `https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`
+    `https://www.googleapis.com/books/v1/volumes?q=${search}&filter=free-ebooks&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`
   );
 
 //=================================
@@ -18,6 +18,12 @@ export const googlebooks = async () =>
 export const getAllNews = async (category, country) =>
   await axios.get(
     `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_NEWS_API}`
+  );
+
+//api for article extraction
+export const articleExtraction = async (data) =>
+  await axios.get(
+    `https://extractorapi.com/api/v1/extractor/?apikey=1e53be0acaeee3b68a36cf3b7e732e3d1d3f93ea&url=${data}`
   );
 
 //================================
